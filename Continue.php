34980a6ba5add90a612d3031879163488efb2c1f
Nikol_eva <Alyona.nikolaeva.88@inbox.ru>
2022-08-13 18:06:34 +0300

    work dev1 stage1

--- Continue.php
+++ Continue.php
@@ -3,3 +3,5 @@ I`m write here working site code. - conflict from Dev1 - second mistake
 I`m write here working site code. - conflict from Dev1 - mistake
 
 Here mistake code.
+
+This is change in branch DEV1 
